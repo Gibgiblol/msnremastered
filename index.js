1eb3ae811e394317076daf6935e462f28da31314
Charles <charlesclei21@gmail.com>
2018-04-08 14:41:14 -0600

    Initial Chat function

--- index.js
+++ index.js
@@ -9,15 +9,76 @@ app.get("/", function (req, resp) {
 var cors = require('cors');    
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+io.on('connection', function (socket) {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+      io.emit('new message', data);
+    // we tell the client to execute 'new message'
+//    socket.broadcast.emit('new message', {
+//      username: socket.username,
+//      message: data
+//    });
+      console.log("message", data);
+  });
+    
+    socket.on('SEND_MESSAGE', function(data){
+        io.emit('RECEIVE_MESSAGE', data);
+    })
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
   });
 });
 
 // Use express to listen to port
-let port = 4000;
-app.listen(process.env.PORT || 4000, function () {
-    console.log("Server running at port= " + port);
+
+http.listen(process.env.PORT || 8080, function () {
+    console.log("Server running at port= " + process.env.PORT);
 });
+
+
+
     
